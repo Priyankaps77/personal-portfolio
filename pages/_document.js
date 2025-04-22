e6a92d69cf9bb6bd33706424d65318718de7d274
priyanka sahu <priyanka.s@techub.in>
2025-04-22 12:26:08 +0530

    doc fixed

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,18 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+// ./pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-function MyDocument() {
-  return (
-    <Html>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Poppins:wght@400;600;700&family=Raleway:wght@400;600;700&family=Ubuntu:wght@400;600;700&family=Lato:wght@400;600;700&family=Open+Sans:wght@400;600;700&family=Inter:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
-export default MyDocument;
+export default MyDocument
